feeebd6eb8cdcfac4dc7731f61b75c36ab2128c3
L-Gaudet <lgaudet@chapman.edu>
2022-08-10 12:53:48 -0700

    adding build

--- src/About.js
+++ src/About.js
@@ -18,7 +18,7 @@ function About() {
               <p className='Bio'>
                 hi, my name is Lucas Gaudet and I'm a computer science major living
                 in southern california. i used to play hockey, but transitioned to 
-                pursue my passion of science and technology. currently I've been interested
+                pursue my passion of science and technology. currently i've been interested
                 in ai/ml engineering, software engineering, and web/app development. 
                 <Link to="/Photos">
                   <button className="Inbio-links">
@@ -27,7 +27,7 @@ function About() {
                 </Link>
                 is a hobby of mine, i carry a camera with me every day for 
                 whenever i see a cool shot. staying active is important to me, whether it
-                be tennis, basketball, hiking or biking I'm always looking for a fun way
+                be tennis, basketball, hiking or biking i'm always looking for a fun way
                 to close the rings on my apple watch.
                 other activities i enjoy are watching hockey, playing games with friends, 
                 and spending time at cool cafes/coffee shops.
